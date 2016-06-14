8a8a3be466748f7702bd00e6a1136be2ef80d231
Dr_Newbie <tf2baidonation@gmail.com>
2016-06-14 20:41:21 +0800

    3. fix game crash when shout others

--- Ask Bot to Carry the Bag/lua/ShoutToPutDownCarry.lua
+++ Ask Bot to Carry the Bag/lua/ShoutToPutDownCarry.lua
@@ -7,7 +7,7 @@ local _f_PlayerStandard_get_intimidation_action = PlayerStandard._get_intimidati
 function PlayerStandard:_get_intimidation_action(prime_target, ...)
 	local voice_type, plural, prime_target = _f_PlayerStandard_get_intimidation_action(self, prime_target, ...)
 	if (voice_type == "come" or voice_type == "boost") and prime_target and prime_target.unit_type == 2 then
-		if prime_target.unit:brain():Get_Carray_Data() then
+		if prime_target.unit and prime_target.unit:brain() and prime_target.unit:brain():Get_Carray_Data() then
 			if mvector3.distance(self._unit:position(), prime_target.unit:position()) <= 500 then
 				prime_target.unit:brain():Drop_Carray()
 			end
