9dd3b9a4035f5180de3672624d70cd19ffd4a161
Dr_Newbie <tf2baidonation@gmail.com>
2016-06-15 15:32:56 +0800

    4.5.1 ; Host peer id is always 1. I guess.

--- Ask Bot to Carry the Bag/lua/HostArrestedSoBotDrop.lua
+++ Ask Bot to Carry the Bag/lua/HostArrestedSoBotDrop.lua
@@ -6,8 +6,7 @@ local _f_GroupAIStateBase_on_player_criminal_death = GroupAIStateBase.on_player_
 
 function GroupAIStateBase:on_player_criminal_death(peer_id)
 	_f_GroupAIStateBase_on_player_criminal_death(self, peer_id)
-	local my_peer_id = managers.network:session():local_peer():id()
-	if peer_id == my_peer_id then
+	if peer_id == 1 then
 		for _, data in pairs(self:all_AI_criminals()) do
 			if data.unit and alive(data.unit) then
 				data.unit:brain():Drop_Carray()
